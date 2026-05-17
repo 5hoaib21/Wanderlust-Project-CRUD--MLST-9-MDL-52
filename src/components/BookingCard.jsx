b12dd4f1e4e96641c8ece99b2fb5b0f29a58ea89
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-05-18 03:34:24 +0600

    Create booking card and send POST API

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -1,21 +1,53 @@
-'use client'
-import { Button, Card, DateField, Label } from '@heroui/react';
-import React from 'react';
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Button, Card, DateField, Label } from "@heroui/react";
+import React, { useState } from "react";
 
-const BookingCard = ({destination}) => {
-  const {price} = destination;
+const BookingCard = ({ destination }) => {
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+  const [departureDate, setDepartureDate] = useState(null);
+  const { price, _id, imageUrl, country, destinationName } = destination;
+
+  const handleBookNow = async () => {
+    const bookingData = {
+      userId: user.id,
+      userImage: user.image,
+      userName: user.name,
+      destinationId: _id,
+      destinationName,
+      price,
+      imageUrl,
+      country,
+      departureDate: new Date(departureDate),
+    };
+    const res = await fetch("http://localhost:8008/booking", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(bookingData),
+    });
+    const data = await res.json();
+    console.log(data, "data from f");
+  };
+
   return (
-    <Card className='rounded-none border mt-5'>
-     <p className='text-sm font-light'>Starting from</p>
-     <h2 className='text-3xl font-bold'>$ {price}</h2>
-     <p className='text-sm font-light'>per person</p>
-     <DateField className="w-[256px]" name="date">
-      <Label>Departure Date</Label>
-      <DateField.Group>
-        <DateField.Input>{(segment) => <DateField.Segment segment={segment} />}</DateField.Input>
-      </DateField.Group>
-    </DateField>
-    <Button className={'w-full'}>Book Now</Button>
+    <Card className="rounded-none border mt-5">
+      <p className="text-sm font-light">Starting from</p>
+      <h2 className="text-3xl font-bold">$ {price}</h2>
+      <p className="text-sm font-light">per person</p>
+      <DateField onChange={setDepartureDate} className="w-[256px]" name="date">
+        <Label>Departure Date</Label>
+        <DateField.Group>
+          <DateField.Input>
+            {(segment) => <DateField.Segment segment={segment} />}
+          </DateField.Input>
+        </DateField.Group>
+      </DateField>
+      <Button onClick={handleBookNow} className={"w-full"}>
+        Book Now
+      </Button>
     </Card>
   );
 };
