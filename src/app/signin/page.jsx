5f01088d484ead99095b370ea1a32c5c02220f1b
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-05-18 00:40:20 +0600

    Implementing login page and showing user data on navbar

---
+++ src/app/signin/page.jsx
@@ -0,0 +1,113 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import {Check} from "@gravity-ui/icons";
+import {Button, Description, FieldError, Form, Input, Label, TextField} from "@heroui/react";
+import { Card } from "@heroui/react";
+import { redirect } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
+
+
+const SignUpPage = () => {
+  const onSubmit = async (e)=> {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const user = Object.fromEntries(formData.entries())
+   
+    const {data, error} = await authClient.signUp.email({
+      email : user.email,
+      name : user.name,
+      password : user.password,
+      image: user.image
+    })
+ 
+
+    
+    if(data){
+      toast.success("welcome Home")
+      redirect('/')
+    }else{
+      toast.error(error.message)
+    }
+  }
+  return (
+    <div className="flex justify-center my-30">
+  
+   
+     <Card>
+      <h1 className="text-2xl font-bold text-center my-2">Create Account</h1>
+      <Form
+      onSubmit={onSubmit}
+       className="flex w-96 flex-col gap-4" >
+      <TextField
+        isRequired
+        name="name"
+        type="text"
+        
+      >
+        <Label>Name</Label>
+        <Input placeholder="Name" />
+        <FieldError />
+      </TextField>
+      <TextField
+       
+        name="image"
+        type="url"
+        
+      >
+        <Label>Image Url</Label>
+        <Input placeholder="Image Url" />
+        <FieldError />
+      </TextField>
+      <TextField
+        isRequired
+        name="email"
+        type="email"
+        validate={(value) => {
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            return "Please enter a valid email address";
+          }
+          return null;
+        }}
+      >
+        <Label>Email</Label>
+        <Input placeholder="john@example.com" />
+        <FieldError />
+      </TextField>
+      <TextField
+        isRequired
+        minLength={8}
+        name="password"
+        type="password"
+        validate={(value) => {
+          if (value.length < 8) {
+            return "Password must be at least 8 characters";
+          }
+          if (!/[A-Z]/.test(value)) {
+            return "Password must contain at least one uppercase letter";
+          }
+          if (!/[0-9]/.test(value)) {
+            return "Password must contain at least one number";
+          }
+          return null;
+        }}
+      >
+        <Label>Password</Label>
+        <Input placeholder="Enter your password" />
+        <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+        <FieldError />
+      </TextField>
+      <div className="flex justify-center gap-2">
+        <Button className={'w-full'} type="submit">
+     
+          Create Account
+        </Button>
+      
+      </div>
+    </Form>
+     </Card>
+     <Toaster />
+    </div>
+  );
+};
+
+export default SignUpPage;
